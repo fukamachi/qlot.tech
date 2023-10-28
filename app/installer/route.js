f55dafa7818bae4129ecec3ff38dadb503d05185
Eitaro Fukamachi <e.arrows@gmail.com>
2023-10-28 15:06:42 +0000

    route.ts -> route.js.

--- app/installer/route.js
+++ app/installer/route.js
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
-export async function GET(request: Request) {
+export async function GET(request) {
   const res = await fetch('https://api.github.com/repos/fukamachi/qlot/releases/latest')
   const data = await res.json();
   const installer = data.assets.find(asset => asset.name.match(/^qlot-installer-.+\.sh$/))
