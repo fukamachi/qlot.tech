2bcdbfb717f67c38cd2381aa32fb8dae35bf30d8
Eitaro Fukamachi <e.arrows@gmail.com>
2023-10-31 13:09:11 +0000

    Expire 'fetch' cache every 1 hour.

--- app/installer/route.js
+++ app/installer/route.js
@@ -1,7 +1,9 @@
 import { redirect } from 'next/navigation'
 
 export async function GET(request) {
-  const res = await fetch('https://api.github.com/repos/fukamachi/qlot/releases/latest')
+  const res = await fetch('https://api.github.com/repos/fukamachi/qlot/releases/latest', {
+    next: { revalidate: 3600 }
+  });
   const data = await res.json();
   const installer = data.assets.find(asset => asset.name.match(/^qlot-installer-.+\.sh$/))
   redirect(installer.browser_download_url)
